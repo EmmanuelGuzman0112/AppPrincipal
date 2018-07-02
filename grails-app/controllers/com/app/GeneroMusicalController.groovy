5f81e00182432235293e72abb47ca1ec92a281c9
Emmanuel <guzman-emmanuel@live.com>
2018-07-02 19:09:42 -0300

    Commit final
    Generate all aplicado.
    
    Relaciones terminadas

--- grails-app/controllers/com/app/GeneroMusicalController.groovy
+++ grails-app/controllers/com/app/GeneroMusicalController.groovy
@@ -1,7 +1,99 @@
 package com.app
 
+import grails.validation.ValidationException
+import static org.springframework.http.HttpStatus.*
+
 class GeneroMusicalController {
 
-    static scaffold = GeneroMusical
+    GeneroMusicalService generoMusicalService
 
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond generoMusicalService.list(params), model:[generoMusicalCount: generoMusicalService.count()]
+    }
+
+    def show(Long id) {
+        respond generoMusicalService.get(id)
+    }
+
+    def create() {
+        respond new GeneroMusical(params)
+    }
+
+    def save(GeneroMusical generoMusical) {
+        if (generoMusical == null) {
+            notFound()
+            return
+        }
+
+        try {
+            generoMusicalService.save(generoMusical)
+        } catch (ValidationException e) {
+            respond generoMusical.errors, view:'create'
+            return
+        }
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'generoMusical.label', default: 'GeneroMusical'), generoMusical.id])
+                redirect generoMusical
+            }
+            '*' { respond generoMusical, [status: CREATED] }
+        }
+    }
+
+    def edit(Long id) {
+        respond generoMusicalService.get(id)
+    }
+
+    def update(GeneroMusical generoMusical) {
+        if (generoMusical == null) {
+            notFound()
+            return
+        }
+
+        try {
+            generoMusicalService.save(generoMusical)
+        } catch (ValidationException e) {
+            respond generoMusical.errors, view:'edit'
+            return
+        }
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'generoMusical.label', default: 'GeneroMusical'), generoMusical.id])
+                redirect generoMusical
+            }
+            '*'{ respond generoMusical, [status: OK] }
+        }
+    }
+
+    def delete(Long id) {
+        if (id == null) {
+            notFound()
+            return
+        }
+
+        generoMusicalService.delete(id)
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'generoMusical.label', default: 'GeneroMusical'), id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'generoMusical.label', default: 'GeneroMusical'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
 }
--- grails-app/controllers/com/app/RolController.groovy
+++ grails-app/controllers/com/app/RolController.groovy
@@ -1,7 +1,99 @@
 package com.app
 
+import grails.validation.ValidationException
+import static org.springframework.http.HttpStatus.*
+
 class RolController {
 
-    static scaffold = Rol
+    RolService rolService
 
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond rolService.list(params), model:[rolCount: rolService.count()]
+    }
+
+    def show(Long id) {
+        respond rolService.get(id)
+    }
+
+    def create() {
+        respond new Rol(params)
+    }
+
+    def save(Rol rol) {
+        if (rol == null) {
+            notFound()
+            return
+        }
+
+        try {
+            rolService.save(rol)
+        } catch (ValidationException e) {
+            respond rol.errors, view:'create'
+            return
+        }
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'rol.label', default: 'Rol'), rol.id])
+                redirect rol
+            }
+            '*' { respond rol, [status: CREATED] }
+        }
+    }
+
+    def edit(Long id) {
+        respond rolService.get(id)
+    }
+
+    def update(Rol rol) {
+        if (rol == null) {
+            notFound()
+            return
+        }
+
+        try {
+            rolService.save(rol)
+        } catch (ValidationException e) {
+            respond rol.errors, view:'edit'
+            return
+        }
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'rol.label', default: 'Rol'), rol.id])
+                redirect rol
+            }
+            '*'{ respond rol, [status: OK] }
+        }
+    }
+
+    def delete(Long id) {
+        if (id == null) {
+            notFound()
+            return
+        }
+
+        rolService.delete(id)
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'rol.label', default: 'Rol'), id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'rol.label', default: 'Rol'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
 }
